a108b3080c426955d96fc8d0ee782e8487766e7a
Shuvo Mallik <shuvomallik999@gmail.com>
2025-11-12 01:06:51 +0600

    complete operation added

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ const client = new MongoClient(uri, {
   }
 });
 
+const calculateStreak = (dates) => {
+  if(!dates.length){
+    return 0;
+  }
+  const sortDate = dates.sort((x,y) => new Date(y) - new Date(x));
+  let streak = 1;
+  for (let i = 1; i < sortDate.length; i++) {
+  const diff = (new Date(sortDate[i - 1]) - new Date(sortDate[i])) / (1000 * 60 * 60 * 24);
+  if (diff === 1) streak++;
+  else break;
+}
+return streak;
+
+}
+
 
 
 
@@ -33,7 +48,7 @@ async function run() {
         res.send('Server is running')
     })
     app.get('/all-habits' ,async(req,res) => {
-      const result = await habitsCollection.find().toArray();
+      const result = await habitsCollection.find().sort({created_at: -1}).toArray();
       res.send(result)
     })
     app.get('/latest-fatures',async(req,res) => {
@@ -104,7 +119,26 @@ async function run() {
       res.send(result)
     })
 
+    app.patch('/habits-complete/:id' , async(req,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const today = new Date().toISOString().split("T")[0];
+      console.log(today)
+      const habit = await habitsCollection.findOne(filter);
+      if(habit.completion_history.includes(today)){
+        return res.status(400).send({message: 'Already completed today!'})
+      }
+      const update = {
+        $push : {completion_history : today}
+      }
+      const result = await habitsCollection.updateOne(filter,update)
+      const updated = await habitsCollection.findOne(filter);
+      const streak = calculateStreak(updated.completion_history);
+      await habitsCollection.updateOne(filter,{$set:{current_streak: streak}})
+      res.send({message: "Marked complete", streak})
+      console.log(result,streak)
 
+    })
 
 
 
